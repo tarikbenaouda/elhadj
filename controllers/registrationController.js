96aa137a5c46229786a97c3e319fa8c08e056b6c
tarikbenaouda <ta.benaouda@esi-sba.dz>
2024-04-29 23:32:54 +0100

    Updated registrationController.register

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,5 +1,6 @@
 const Registration = require('../models/registrationModel');
 const RegistrationHistory = require('../models/registrationHistoryModel');
+const Algo = require('../models/algorithmModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllRegistrations = catchAsync(async (req, res, next) => {
@@ -13,16 +14,22 @@ exports.getAllRegistrations = catchAsync(async (req, res, next) => {
   });
 });
 exports.register = catchAsync(async (req, res, next) => {
-  const defaultCoefficient = 1;
-  const ageLimitToApply = 60;
-  const ageCoefficient = 1;
-  const registrationCoefficient = 1;
+  const {
+    defaultCoefficient,
+    ageLimitToApply,
+    ageCoefficient,
+    registerCoefficient,
+  } = await Algo.findOne({});
+  // const defaultCoefficient = 1;
+  // const ageLimitToApply = 60;
+  // const ageCoefficient = 1;
+  // const registrationCoefficient = 1;
   const registrations = await RegistrationHistory.getRegistrationsNumber(
     req.user.id,
   );
   const coefficient =
     defaultCoefficient +
-    registrations * registrationCoefficient +
+    registrations * registerCoefficient +
     (req.user.calculateAge() > ageLimitToApply ? ageCoefficient : 0);
 
   const registration = await Registration.create({
